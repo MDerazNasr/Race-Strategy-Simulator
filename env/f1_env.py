8c9c4941a5a96316e5246dc4b7926f872636dbd7
Mohamed Deraz Nasr <mderaznasr@gmail.com>
2025-11-19 18:28:17 -0500

    constructor for gym class

--- env/f1_env.py
+++ env/f1_env.py
@@ -13,15 +13,15 @@ from .car_model import Car
 from .track import generate_oval_track, closest_point, progress_along_track
 
 class F1Env(gym.Env):
-    metadata = {"render_modes": ["human"], "render_fps": 30}
+    metadata = {"render_modes": ["human"], "render_fps": 30} #info used for rendering
 
     def __init__(self, render_mode=None, dt= 0.1):
         super().__init__()
-        super.dt = dt
-        self.render_mode = render_mode
+        super.dt = dt #dt = how much simulated time passes each step(). Same as in Car
+        self.render_mode = render_mode #render_mode = you'll use this later if you draw stuff
 
         #Track
-        self.track = generate_oval_track()
+        self.track = generate_oval_track() #you generate a simple circular-ish track (list of (x,y) points)
 
         #Car
         self.car = Car()
@@ -33,6 +33,18 @@ class F1Env(gym.Env):
         )
 
         #Observations: [x,y, cos(yaw), sin(yaw), v ]
+        obs_high = np.array(
+            [1000, 1000, 1, 1, 100, 100, 1],
+            dtype=np.float32
+        )
+        self.observation_space = spaces.Box(
+            low = obs_high,
+            high = obs_high,
+            dtype = np.float32
+        )
+        self.max_steps = 2000
+        self.step_count = 0
+        
 '''
 Gym gives a standard interface:
 	•	reset() → start a new episode → returns initial observation
