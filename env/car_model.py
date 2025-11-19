04ffdb13938d4939446acd7ea4550baec0c3a107
Mohamed Deraz Nasr <mderaznasr@gmail.com>
2025-11-19 01:54:03 -0500

    added constructor for car class

--- env/car_model.py
+++ env/car_model.py
@@ -3,12 +3,37 @@
 import numpy as np
 
 class Car:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.yaw = 0
-        self.v = 0
-    
+    # storing the car's state and its physical limits
+    #creating function step to update the car's state
+    def __init__(
+        self,
+        x=0.0,
+        y=0.0,
+        yaw=0.0,
+        v=0.0,
+        wheelbase=2.5,
+        max_steer=np.deg2rad(30),
+        max_accel=5.0,
+        max_decel=-8.0,
+        drag_coeff=0.1,
+    ):
+        self.x = x
+        self.y = y
+        self.yaw = yaw
+        self.v = v
+
+        self.wheelbase = wheelbase
+        self.max_steer = max_steer
+        self.max_accel = max_accel
+        self.max_decel = max_decel
+        self.drag_coeff = drag_coeff
+
+    def rest(self, x=0.0, y=0.0, yaw=0.0, v=0.0):
+        self.x = x
+        self.y = y
+        self.yaw = yaw
+        self.v = v
+
     def step(self, throttle, steering, dt=0.1):
         #implement physics
         #this will becomne RL environment
