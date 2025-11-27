ecf2417bcb6584edc7c433df5ea4214c7f52baef
Mohamed Deraz Nasr <mderaznasr@gmail.com>
2025-11-26 20:47:12 -0500

    implementing expert contsrctor

--- expert/expert_driver.py
+++ expert/expert_driver.py
@@ -1,2 +1,15 @@
 """Expert driver policy for generating demonstrations."""
 
+import numpy
+from env.track import closest_point
+
+class ExpertDriver:
+    def __init__(self, track, lookahead=8, max_speed=20.0, corner_factor=12.0):
+        self.track = track
+        self.lookahead = lookahead
+        self.max_speed = max_speed
+        self.corner_factor = corner_factor
+    
+    
+        
+             
